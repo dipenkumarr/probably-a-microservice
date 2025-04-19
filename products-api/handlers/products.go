1189f3929d6c5031ff59c9a353a9d573abb9b2e1
Dipen Kumar <dipenbachani@gmail.com>
2025-04-18 21:47:37 -0700

    update handlers for add and update operations

--- products-api/handlers/products.go
+++ products-api/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/dipenkumarr/probably-a-microservice/data"
 )
@@ -21,13 +23,73 @@ func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(w, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		rgx := regexp.MustCompile(`/([0-9]+)`)
+		g := rgx.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, _ := strconv.Atoi(idString)
+
+		p.updateProduct(id, w, r)
+		return
+	}
+
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
 	if err != nil {
-		http.Error(w, "Unable to marshal JSON", http.StatusInternalServerError)
+		http.Error(w, "Unable to encode JSON", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, "Wrong Id", http.StatusBadRequest)
+	}
+
 }
